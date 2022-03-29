2c442162442b753b8f7fa19537d700adaa1956f9
Alex King <calicoalix@gmail.com>
2022-03-29 23:06:25 +1100

    skills fade in

--- js/scroll-track.js
+++ js/scroll-track.js
@@ -16,7 +16,31 @@ $(window).scroll(function(){
         $(".nav-contact").children().removeClass('selected')
         // $(".my-skills").addClass('drop')
         // $(".my-projects").addClass('drop')
+        $(".html").addClass("drop1")
+        $(".rails").addClass("drop1")
+        $(".express").addClass("drop1")
+        $(".heroku").addClass("drop1")
 
+        $(".css").addClass("drop2")
+        $(".jquery").addClass("drop2")
+        $(".node").addClass("drop2")
+        $(".git").addClass("drop2")
+
+        $(".javascript").addClass("drop3")
+        $(".react").addClass("drop3")
+        $(".three").addClass("drop3")
+        $(".github").addClass("drop3")
+
+        $(".ruby").addClass("drop4")
+        $(".vue").addClass("drop4")
+        $(".go").addClass("drop4")
+        $(".netlify").addClass("drop4")
+
+        $(".mongo").addClass("drop5")
+        $(".postgres").addClass("drop5")
+
+
+        // $(".html").addClass("drop1")
     } else if ($("#projects").offset().top > endLine) {
         $(".nav-about").children().removeClass('selected')
         $(".nav-skills").children().removeClass('selected')
