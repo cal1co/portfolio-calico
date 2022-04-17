6fecc28fad7034f49fe59d6f0f5299d222133a8f
Alex King <calicoalix@gmail.com>
2022-04-17 23:31:19 +1000

    remove the console logs for the loading of llama

--- js/grass.js
+++ js/grass.js
@@ -932,11 +932,11 @@ llamaLoader.load(
 	},
 	// called while loading is progressing
 	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		// console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 	},
 	// called when loading has errors
 	function ( error ) {
-		console.log( 'An error happened' );
+		// console.log( 'An error happened' );
 	}
 );
 
